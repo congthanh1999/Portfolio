33f4ceedc065125cf66d5c169b193243ecf514ca
congthanh1999 <42653354+congthanh1999@users.noreply.github.com>
2023-09-09 18:24:48 +0700

    editing menu-icon click event

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('.header .navbar a');
-let navbar = document.querySelector('header');
+let header = document.querySelector('header');
+
+let navbar = document.querySelector('.navbar');
+let menuIcon = document.querySelector('#menu-icon');
 
 window.onscroll = () => {
     sections.forEach(section => {
-        let top = window.scrollY + navbar.offsetHeight;
+        let top = window.scrollY + header.offsetHeight;
         let offset = section.offsetTop;
         let height = section.offsetHeight;
         let id = section.getAttribute('id');
@@ -16,5 +19,13 @@ window.onscroll = () => {
             });
         };
     });
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
 };
 
